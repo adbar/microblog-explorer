4379498f63cd81a46be7e4d46e96f2be9561dbd9
Adrien Barbaresi <adrien.barbaresi@ens-lyon.fr>
2012-08-14 23:19:38 +0200

    options and args, percentage stats, unique links

--- langid/lang-stats+selection.py
+++ langid/lang-stats+selection.py
@@ -6,33 +6,57 @@
 
 # This script is to be used with the output of a language identification system (https://github.com/saffsd/langid.py).
 
-
+from __future__ import division
+from __future__ import print_function
 from collections import defaultdict
 
+import optparse
+
+## Parse arguments and options
+parser = optparse.OptionParser(usage='usage: %prog [options] arguments')
+
+parser.add_option('-lc', '--language-codes',
+	action="store_true", dest="lcodes", default=False,
+	help="be prompted for language codes to show")
+parser.add_option("-f", "--file", dest="filename",
+	help="input file name", metavar="FILE")
+
+options, args = parser.parse_args()
+
+if options.filename is None:
+	parser.error('No filename given')
+
+## Initialize
 langd = defaultdict(int)
 urld = defaultdict(int)
 
-inp = raw_input('Languages wanted (comma-separated codes) : ')
-langlist = inp.split(',')
+if options.lcodes is True:
+	inp = raw_input('Languages wanted (comma-separated codes) : ')
+	langlist = inp.split(',')
 
-f = open('sample', 'r')
+f = open(options.filename, 'r')
 
+
+## Parse input file
 for line in f:
 	columns = line.split('\t')
 	if len(columns) == 3:
-		langd[columns[1]] += 1
-		urld[columns[0]] = columns[1]
-
+		if columns[0] not in urld:
+			langd[columns[1]] += 1
+			urld[columns[0]] = columns[1]
 f.close()
 
+
+## Display and print the results
+print (len(urld), 'total unique urls')
 for l in sorted(langd, key=langd.get, reverse=True):
-	print l, langd[l]
+	pcent = (langd[l] / len(urld))*100
+	print (l, langd[l], '%.1f' % round(pcent, 1), sep='\t')
 
-out = open('sample-choice', 'w')
-
-for lang in langlist:
-	for key in urld:
-		if urld[key] == lang:
-			out.write(key + '\t' + urld[key] + '\n') 
-
-out.close()
+if options.lcodes is True:
+	out = open('sample-choice', 'w')
+	for lang in langlist:
+		for key in urld:
+			if urld[key] == lang:
+				out.write(key + '\t' + urld[key] + '\n') 
+	out.close()
